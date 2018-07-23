d48b8a655c6c76920fdd90029e201aa789903762
Cook1619 <cook6348@gmail.com>
2018-07-23 07:48:27 -0500

    Messing with views and padding

--- App.js
+++ App.js
@@ -6,24 +6,16 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={{ flex: 1, backgroundColor: 'orange' }}></View>
+        <View style={{ flex: 1, backgroundColor: 'red' }}></View>
+        <View style={{ flex: 1, backgroundColor: 'teal' }}></View>
       </View>
     );
   }
@@ -31,19 +23,9 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
+    padding: 10,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
